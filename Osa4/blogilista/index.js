19571fa14cfbd3b385dd084d8eec469bc232b4c7
savisulo <suvi.j.salo@gmail.com>
2023-05-14 20:47:13 +0300

    Osa4 tehtävä 4.1 step1 toimii

---
+++ Osa4/blogilista/index.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const Blog = require('./models/blog')
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.get('/api/blogs/:id', (request, response) => {
+    Blog.findById(request.params.id)
+        .then(blog => {
+            if (blog) {
+                response.json(blog)
+            } else {
+                response.status(404).end()
+            }
+        })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
